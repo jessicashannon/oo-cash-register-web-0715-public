a93472cc981d2dfb4b46799df3193efd4311738f
jessicashannon <jessdahling@gmail.com>
2015-08-05 17:28:21 -0400

    done

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+class CashRegister
+
+  attr_reader :total
+
+  def initialize(employee_discount=nil)
+    @total = 0
+    @discount = employee_discount
+    @items = []
+  end
+
+  def discount
+    @discount
+  end
+
+  def total
+    @total
+  end
+
+  def total=(total)
+    @total = total
+  end
+
+  def add_item(title, price, quantity=1)
+    @total = total + (price * quantity)
+    quantity.times do
+      @items << title
+    end
+  end
+
+  def apply_discount
+    if @discount == nil
+      "There is no discount to apply."
+    else
+      @total = @total - (@total * @discount/100)
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def items
+    @items
+  end
+
+end
